2b31bf4f724fa765c775680d55645f96288e61c7
n8659150 <jackli900421@hotmail.com>
2020-07-11 22:57:51 +0800

    配置request相关文件

--- src/components/Beauty/index.jsx
+++ src/components/Beauty/index.jsx
@@ -1,4 +1,9 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useRequest } from '@umijs/hooks';
+import { URL, port, apiRoute } from '../../config';
+const fullURL = `http://${URL}:${port}/${apiRoute}`;
 export function Beauty(props) {
+    const { data, error, loading } = useRequest(`${fullURL}=285906324`);
+    if (data) console.log(data.imgSrc);
     return <div>{props.qid}</div>
 }
---
+++ src/config.js
@@ -0,0 +1,7 @@
+export const URL = '106.14.251.55';
+export const port = '8089';
+export const apiRoute = 'sisterImg?questionId';
+
+
+
+
